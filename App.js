b8cecd14bddc36a62a55978c3587ff16f796dc9f
vkpmind <vinodkumarpujar@gmail.com>
2023-11-24 03:01:02 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,7 @@
+let heading = React.createElement(
+  "h1",
+  { id: "react-id", className: "react-class" },
+  "I am from React JS..."
+);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
